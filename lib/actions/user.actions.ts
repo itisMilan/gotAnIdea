fb4c8f0658eeac2a5db650eee8a5efd98382050f
milan <abhaymilan8@gmail.com>
2023-09-14 16:45:47 +0530

    updated the user actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,85 +4,166 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder, isValidObjectId } from "mongoose";
 
 
- interface Params{
-    userId:string;
-    username:string,
-    name:string,
-    bio:string,
-    image:string,
-    path:string
- }
+interface Params {
+    userId: string;
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string
+}
 export async function updateUser({
 
-    userId ,
-    username, 
-    name, 
-    bio, 
-    image, 
-    path, 
-}:Params
+    userId,
+    username,
+    name,
+    bio,
+    image,
+    path,
+}: Params
 ): Promise<void> {
     connectToDB();
-try{
+    try {
 
-    
-    await User.findOneAndUpdate({ id: userId },
-        {
-            username: username.toLowerCase(),
-            name,
-            bio,
-            image,
-            onboarded: true,
-        }, { upsert: true }
+
+        await User.findOneAndUpdate({ id: userId },
+            {
+                username: username.toLowerCase(),
+                name,
+                bio,
+                image,
+                onboarded: true,
+            }, { upsert: true }
         );
-        if(path==="/profile/edit"){
+        if (path === "/profile/edit") {
             revalidatePath(path);
         }
-}
-catch (error:any){
-   throw new Error(`Failed to create/update user:${error.message}`) 
-}
+    }
+    catch (error: any) {
+        throw new Error(`Failed to create/update user:${error.message}`)
+    }
 }
 
-export async function fetchUser(userId:string){
-    try{
+export async function fetchUser(userId: string) {
+    try {
         connectToDB();
-       return await User
-       .findOne({id:userId})
-    //    .populate({
-    //     path:'communities',
-    //     model: Community
-    //    })
+        return await User
+            .findOne({ id: userId })
+        //    .populate({
+        //     path:'communities',
+        //     model: Community
+        //    })
     }
-    catch (error:any){
-        throw new Error (`Failed to fetch user:${error.message}`)
+    catch (error: any) {
+        throw new Error(`Failed to fetch user:${error.message}`)
     }
 }
 
 
-export async function fetchUserPosts(userId:string){
-    try{
-       connectToDB();
+export async function fetchUserPosts(userId: string) {
+    try {
+        connectToDB();
 
-       const threads=await User.findOne({id:userId})
-       .populate({
-        path:"threads",
-        model:Thread,
-        populate:{
-            path:'children',
-            model:Thread,
-            populate:{
-                path:"author",
-                model:User,
-                select:'name image id'
-            }
+        const threads = await User.findOne({ id: userId })
+            .populate({
+                path: "threads",
+                model: Thread,
+                populate: {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: "author",
+                        model: User,
+                        select: 'name image id'
+                    }
+                }
+            })
+        return threads;
+    }
+    catch (error: any) {
+        throw new Error(`Failed to fetch user posts: ${error.message}`)
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString = "",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc"
+}: {
+    userId: string;
+    searchString?: string;
+    pageNumber: number;
+    pageSize: number;
+    sortBy?: SortOrder;
+
+}) {
+    try {
+        connectToDB();
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query: FilterQuery<typeof User> = {
+            id: { $ne: userId }
         }
-       })
-       return threads;
+        if (searchString.trim() !== "") {
+            query.$or = [
+                { username: { $regex: regex } },
+                { name: { $regex: regex } }
+            ]
+        }
+        const sortOptions = { createdAt: sortBy };
+
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return { users, isNext };
     }
-    catch(error:any){
-throw new Error(`Failed to fetch user posts: ${error.message}`)
+    catch (error: any) {
+        throw new Error(`Failed to fetch Users:${error.message}`)
     }
 }
+export async function getActivity(userId: string) {
+    try {
+      connectToDB();
+      if (!isValidObjectId(userId)) {
+        throw new Error('Invalid user ID');
+      }
+  
+      // Find all threads created by the user
+      const userThreads = await Thread.find({ author: userId });
+  
+      // Collect all the child thread ids (replies) from the 'children' field of each user thread
+      const childThreadIds = userThreads.reduce((acc, userThread) => {
+        return acc.concat(userThread.children);
+      }, []);
+  
+      // Find and return the child threads (replies) excluding the ones created by the same user
+      const replies = await Thread.find({
+        _id: { $in: childThreadIds },
+        author: { $ne: userId }, // Exclude threads authored by the same user
+      }).populate({
+        path: "author",
+        model: User,
+        select: "name image _id",
+      });
+  
+      return replies;
+    } catch (error) {
+      console.error("Error fetching replies: ", error);
+      throw error;
+    }
+  }
